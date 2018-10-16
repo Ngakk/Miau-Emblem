9e2129f53056398dae9c0b828902556d6efd2dd3
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-10-16 16:08:51 -0500

    Make a main algorithm

---
+++ Assets/_game/_IMPORTANT/Main_Algorithm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_MainAlgorithm", menuName = "Azareth/Stolen", order = 1)]
+public class Main_Algorithm : ScriptableObject
+{
+	public int[,,] matrix;
+	public int filas;
+	public int columnas;
+
+	public void resizeMatrix()
+	{
+		if(filas < 0 || columnas < 0)
+		{
+			Debug.Log("No uses valores negativos");
+			return;
+		}
+		else if(filas == 0 || columnas == 0) 
+		{
+			Debug.Log("No poner valores de 0");
+		}
+		else if(filas != 0 && columnas != 0)
+		{
+			matrix = new int[filas, columnas, 3];
+			Debug.Log("Matrix resized");
+		}
+	}
+}
---
+++ Assets/_game/_IMPORTANT/New_Inspector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Main_Algorithm))]
+public class ObjectBuilderEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		Main_Algorithm mainCosa = (Main_Algorithm)target;
+		if(GUILayout.Button("Resize Matrix"))
+		{
+			mainCosa.resizeMatrix();
+		}
+	}
+}
