f5e3bf2189ec58216bd6ff6132b48647e4dc4f0c
Ngakk <ngakwastaken@gmail.com>
2018-10-18 08:54:02 -0500

    rangos, battle static y anims

--- Assets/_game/Characters/Scripts/Character.cs
+++ Assets/_game/Characters/Scripts/Character.cs
@@ -8,14 +8,13 @@ namespace Mangos {
         public CharacterStats stats;
 
         public Animator anim;
-        public Animation walk;
         public GameEvent charaWalkFinish;
 
         public float walkSpeed;
         public float walkRotationSpeed;
         public float walkAnimSpeedRatio;
 
-        void Start() {
+        void Start() { 
 
         }
 
@@ -33,7 +32,7 @@ namespace Mangos {
         public void Move(Vector3[] path)
         {
             StartCoroutine("Walk", path);
-            walk.["Walk"].speed = walkAnimSpeedRatio * walkSpeed;
+            StartWalkAnim();
         }
 
 
@@ -73,17 +72,19 @@ namespace Mangos {
                 }
                 yield return null;
             }
-            charaWalkFinish.Raise();
+            EndWalkAnim();
         }
 
         private void StartWalkAnim()
         {
-
+            anim.SetBool("Walking", true);
+            anim.speed = walkSpeed * walkAnimSpeedRatio;
         }
 
         private void EndWalkAnim()
         {
-
+            charaWalkFinish.Raise();
+            anim.SetBool("Walking", false);
         }
     }
 }
--- Assets/_game/Characters/Scripts/CharacterStats.cs
+++ Assets/_game/Characters/Scripts/CharacterStats.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 namespace Mangos
 {
+    public enum CharacterClass : int {
+        WARRIOR,
+        HEALER,
+        ARCHER,
+        MAGE
+    }
+
     [CreateAssetMenu()]
     public class CharacterStats : ScriptableObject {
+        public CharacterClass charClass;
         public int hp;
         public int atk;
         public int def;
         public int spd;
         public int res;
+        public int[] attackRanges; //At wich ranges can it attack
+        public int[] counterAttackRanges; //At wich ranges can it counter attack
         public WeaponStats weapon;
         public int team;
         public int walkRange;
