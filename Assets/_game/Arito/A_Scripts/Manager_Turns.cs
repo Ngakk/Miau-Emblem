a9336bcde5aba1594e76a332d04e411269148eec
Retus001 <retus001@gmail.com>
2018-10-23 16:58:45 -0500

    Movement

---
+++ Assets/_game/Arito/A_Scripts/Manager_Turns.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class Manager_Turns : MonoBehaviour
+    {
+        public GameState currentGameState;
+
+        public void ToggleTurn()
+        {
+
+            currentGameState = GameState.TRANSITION;
+            
+        }
+
+    }
+}
--- Assets/_game/Arito/A_Scripts/PlayerSelection.cs
+++ Assets/_game/Arito/A_Scripts/PlayerSelection.cs
@@ -9,27 +9,68 @@ public class PlayerSelection : MonoBehaviour {
     public Vector2 targetPosition;
     public bool selected;
     public GameObject selectedCharacter;
+    public float rayDistance = 50.0f;
 
-    public void OnCellClick(float x, float y)
+    private void Update()
     {
-        grid.GetComponent<Grid>().WorldToCell(new Vector3(x, y, 0));
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnCellClick(Input.mousePosition);
+        }
+    }
+
+    public void OnCellClick(Vector3 mousePos)
+    {
+        /// Grid Based
+
+        //grid.GetComponent<Grid>().WorldToCell(new Vector3(x, y, 0));
         /* if (getTerrain(x, y)) {
          *     selectedCharacter = getCharacterMatrix(x, y);
          *     selected = true;
          * }
          */
+
+        // TODO
+        // Check Raycast to Floor (layermask)
+        // move character with Characterscript.move(vector3[])
+
+        /// Raycast
+        
+        // if (selectedCharacter.getComponent<stats>().canMove)
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast (ray, out hit, rayDistance))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log(hit.point);
+            Debug.Log(hit.collider.gameObject.name);
+
+            if (!selected && hit.collider.gameObject.CompareTag("Character"))
+            {
+                Debug.Log("Selected " + hit.collider.gameObject.name);
+                selectedCharacter = hit.collider.gameObject;
+                selected = true;
+            }
+            else if (selected && (grid.GetComponent<Grid>().WorldToCell(hit.point).x <= 3 && grid.GetComponent<Grid>().WorldToCell(hit.point).z <= 3))
+            {
+                MoveCharacter(hit.point);
+            }
+        }
     }
 
-    public void MoveCharacter(float x, float y)
+    public void MoveCharacter(Vector3 targetPos)
     {
-        if (selected)
-        {
-            /*
-            targetPosition = new Vector2(x, y);
-            if (getMovementMatrix()[][] <= selectedCharacter.getComponent<CharacterScript>().GetMovementRange()){
-                CallMoveCharacter(targetPosition);
-            }
-            */
+        /*
+        targetPosition = new Vector2(x, y);
+        if (getMovementMatrix()[][] <= selectedCharacter.getComponent<CharacterScript>().GetMovementRange()){
+            CallMoveCharacter(targetPosition);
         }
+        */
+
+        /// Movement
+
+        selectedCharacter.transform.position = targetPos;
+        selectedCharacter = null;
+        selected = false;
     }
 }
