54d0e4f274966faea6b78d41a31673e56e45a243
Galaxyben <galaxyben1@gmail.com>
2018-10-25 08:46:43 -0500

    Something

--- Assets/_game/AI/EnemieManager.cs
+++ Assets/_game/AI/EnemieManager.cs
@@ -6,12 +6,41 @@ namespace Mangos
 {
     public class EnemieManager : MonoBehaviour
     {
-        public int enemieId;
-        public string enemieClass;
+        public Character myCharacter;
+        public CharacterStats myStats;
+        public Main_Algorithm mainA;
+
+        private List<GameObject> enemiesInRange;
 
         private void Start()
         {
             
         }
+
+        public void CheckForAllies()
+        {
+            for(int i = 0; i < myStats.walkRange; i++)
+            {
+                for(int j = 0; j < myStats.walkRange; j++)
+                {
+                    if(mainA.GetCharacterDataAt(i, j) != null)
+                    {
+                        enemiesInRange.Add(mainA.GetCharacterDataAt(i,j));
+                    }
+                }
+            }
+
+            if(enemiesInRange.Count == 0)
+            {
+
+            }
+        }
+
+        public void SendMoveToNearestAlly()
+        {
+
+        }
+
+        public void SendMoveTo
     }
 }
--- Assets/_game/ManagerOperatingScripts/Manager_AIMethodCaller.cs
+++ Assets/_game/ManagerOperatingScripts/Manager_AIMethodCaller.cs
@@ -6,39 +6,21 @@ namespace Mangos
 {
     public class Manager_AIMethodCaller : MonoBehaviour
     {
-        private int[] EnemieTeamNumber; //Con esta variable se usará cómo ID de los enemigos
-        [Tooltip("Ingresa el evento de charWalkFinish")]
-        public GameEvent charWalkFinish;    //Es para avisar de que ya terminó de caminar
-        public Grid MapGrid;
         public Main_Algorithm mainA;
 
         public void Awake()
         {
             Manager_Static.aiMethodCaller = this;
-            CheckEnemiesPosition();
-        }
-
-        public void EnemieManager()
-        {
-            
         }
 
         public Vector3 PositionToMove(Vector3 _charPos)
         {
-            Vector3 myPos = _charPos;
-            
-            return Vector3.zero;
+
         }
 
-        public Vector3 CheckEnemiesPosition()
+        public Vector3 CheckAllyPosition()
         {
-            for (int i = 0; i < mainA.characters.Length; i++)
-            {
-                Debug.Log(i);
-                if()
-                return Vector3.zero;
-            }
-            return Vector3.zero;
+
         }
     }
 }
