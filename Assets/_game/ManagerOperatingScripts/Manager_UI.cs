f58a510d02f497bb8acc54c6bf2460026fc481e0
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-11-08 07:37:02 -0600

    Add Managers UI methods

--- Assets/_game/ManagerOperatingScripts/Manager_UI.cs
+++ Assets/_game/ManagerOperatingScripts/Manager_UI.cs
@@ -36,8 +36,6 @@ namespace Mangos
 		public Text eHP;
 		public Text eATK;
 
-
-
 		void Start()
 		{
 			Manager_Static.uiManager = this;
@@ -47,24 +45,59 @@ namespace Mangos
 		{
 			generalData.SetActive(true);
 			anotherData.SetActive(true);
+			//characterName.text = _character.GetComponent<Character>().namae;
+			characterHP.text =  _character.GetComponent<Character>().stats.hp.ToString() + "/" + _character.GetComponent<Character>().stats.maxHp.ToString();
+			//characterIcon.sprite = _character.GetComponent<Character>().icon;
+			characterATK.text = _character.GetComponent<Character>().stats.atk.ToString();
+			characterDEF.text = _character.GetComponent<Character>().stats.def.ToString();
+			characterRES.text = _character.GetComponent<Character>().stats.res.ToString();
+			characterSPD.text = _character.GetComponent<Character>().stats.spd.ToString();
+
 		}
 
 		public void releaseDataCharacter()
 		{
 			generalData.SetActive(false);
 			anotherData.SetActive(false);
+			//characterName.text = "";
+			characterHP.text =  "";
+			//characterIcon.sprite = "";
+			characterATK.text = "";
+			characterDEF.text = "";
+			characterRES.text = "";
+			characterSPD.text = "";
 		}
 
 		public void getDataCombat(GameObject _alie, GameObject _enemy)
 		{
 			alieData.SetActive(true);
 			enemyData.SetActive(true);
+			//Alie
+			//aIcon.sprite = _alie.GetComponent<Character>().icon; 
+			//aName.text = _alie.GetComponent<Character>().namae;
+			aHP.text = _alie.GetComponent<Character>().stats.hp.ToString();
+			aATK.text = _alie.GetComponent<Character>().stats.atk.ToString();
+			//Enemy
+			//eIcon.sprite = _enemy.GetComponent<Character>().icon; 
+			//eName.text = _enemy.GetComponent<Character>().namae;
+			eHP.text = _enemy.GetComponent<Character>().stats.hp.ToString();
+			eATK.text = _enemy.GetComponent<Character>().stats.atk.ToString();
 		}
 
 		public void releaseDataCombat()
 		{
 			alieData.SetActive(false);
 			enemyData.SetActive(false);
+			//Alie
+			//aIcon.sprite = ""; 
+			//aName.text = "";
+			aHP.text = "";
+			aATK.text = "";
+			//Enemy
+			//eIcon.sprite = "";
+			//eName.text = "";
+			eHP.text = "";
+			eATK.text = "";
 		}
 	}
 }
