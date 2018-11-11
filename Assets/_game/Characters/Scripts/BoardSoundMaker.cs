543e74cf1a0d752a7f2142f6cef8d7368ac40285
Ngakk <32682022+Ngakk@users.noreply.github.com>
2018-11-10 23:00:00 -0600

    Sonidos en animaciones

---
+++ Assets/_game/Characters/Scripts/BoardSoundMaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class BoardSoundMaker : MonoBehaviour
+    {
+        public void OnStep()
+        {
+            Manager_Static.audioManager.PlaySoundAt(transform.position, sounds.STEP);
+        }
+    }
+}
