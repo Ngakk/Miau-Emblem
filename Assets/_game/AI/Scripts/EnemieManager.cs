108a790508c4e8ad99f532522d04af9bcaf71a8a
Ngakk <32682022+Ngakk@users.noreply.github.com>
2018-11-12 01:19:03 -0600

    sdfgsfg

--- Assets/_game/AI/Scripts/EnemieManager.cs
+++ Assets/_game/AI/Scripts/EnemieManager.cs
@@ -69,7 +69,11 @@ namespace Mangos
 
             eStats.CheckForAllies();
             Vector3Int something = grid.WorldToCell(eStats.LookForClosestAlly(enemies[currentEnemy].GetComponent<Character>().transform.position));
-            moveEnemy(something);
+            int[,] tempMatrix = eStats.mainA.ViewMove(enemies[currentEnemy].GetComponent<Character>().coordinates.x, enemies[currentEnemy].GetComponent<Character>().coordinates.y);
+            if (tempMatrix[something.x, something.y] <= enemies[currentEnemy].GetComponent<Character>().stats.walkRange)
+                moveEnemy(something);
+            else
+                NextCharacter();
         }
 
         public void moveEnemy(Vector3Int pos)
@@ -92,8 +96,10 @@ namespace Mangos
                 if (enemies[currentEnemy].GetComponent<Character>().stats.charClass == CharacterClass.HEALER)
                     Manager_Static.battles.HealItOut(enemies[currentEnemy].GetComponent<Character>(), enemies[Random.Range(1, 2)].GetComponent<Character>());
                 else
-                    Manager_Static.battles.DukeItOut(enemies[currentEnemy].GetComponent<Character>(), eStats.mainA.GetCharacterDataAt(enemyToAttack.x, enemyToAttack.y).GetComponent<Character>());
+                {
 
+                    Manager_Static.battles.DukeItOut(enemies[currentEnemy].GetComponent<Character>(), eStats.mainA.GetCharacterDataAt(enemyToAttack.x, enemyToAttack.y).GetComponent<Character>());
+                }
                 if (currentEnemy >= enemies.Length)
                     turnEnded();
             }
